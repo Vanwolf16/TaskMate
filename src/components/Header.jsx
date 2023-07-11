9bea3a6f6e69e8d54ea35802490e3e5c8617483a
vanwolf16 <d.murillo28@yahoo.com>
2023-07-11 12:31:13 -0700

    Change Theme Background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from '../assets/logo.svg'
 
 function Header() {
+  const [theme,setTheme] = useState(JSON.parse(localStorage.getItem("theme")) || "light");
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  },[theme])
+
   return (
     <header>
       <div className='logo'>
@@ -10,12 +18,12 @@ function Header() {
       </div>
 
       <div className='themeSelector'>
-          <span className='light'></span>
-          <span className='medium'></span>
-          <span className='dark'></span>
-          <span className='gradientOne'></span>
-          <span className='gradientTwo'></span>
-          <span className='gradientThree'></span>
+          <span onClick={() => setTheme("light")} className={theme === "light" ? "light activeTheme" : "light"}></span>
+          <span onClick={() => setTheme("medium")} className={theme === "medium" ? "medium activeTheme" : "medium"}></span>
+          <span onClick={() => setTheme("dark")} className={theme === "dark" ? "dark activeTheme" : "dark"}></span>
+          <span onClick={() => setTheme("gradientOne")} className={theme === "gradientOne" ? "gradientOne activeTheme" : "gradientOne"}></span>
+          <span onClick={() => setTheme("gradientTwo")} className={theme === "gradientTwo" ? "gradientTwo activeTheme" : "graidentTwo"}></span>
+          <span onClick={() => setTheme("gradientThree")} className={theme === "gradientThree" ? "gradientThree activeTheme" : "gradientThree"}></span>
       </div>
 
     </header>
